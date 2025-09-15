0d03cdba9312d460321e8c7a1ee3dfa318b92825
Marc-AntoineJobin <2356735@csfoy.ca>
2025-09-15 18:38:03 -0400

    added TSQL project

--- src/Projets.tsx
+++ src/Projets.tsx
@@ -1,8 +1,9 @@
+import TSQL from './assets/T-SQL.png'
 function Projets() {
     return (
     <>
     <h1>Projets</h1>
-    <p>A venir...</p>
+    <img src={TSQL} className='mt-2 rounded' style={{ maxWidth: '300px' }} alt="marc-Antoine Jobin"/>
 </>
     )
 }
